85565dfe4e3a4b7ece89f11137940221888f7b06
csy <1608088355@qq.com>
2022-05-01 16:29:10 +0800

    5-01

---
+++ leetcode/src/LeetCode1305.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Csy
+ * @Classname LeetCode1305
+ * @date 2022-05-01 16:26
+ * @Description TODO
+ */
+public class LeetCode1305 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Solution1305 {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> ret = new ArrayList<>();
+        dfs(root1, ret);
+        dfs(root2, ret);
+        Collections.sort(ret);
+        return ret;
+    }
+
+    private void dfs(TreeNode root, List<Integer> ret) {
+        if (root != null) {
+            dfs(root.left, ret);
+            ret.add(root.val);
+            dfs(root.right, ret);
+        }
+    }
+}
